79c9c9e0aa0d2709893d3d8edc63daed5d8b6b95
Tas812 <tasnimfrikha@gmail.com>
2024-05-03 22:35:16 +0100

    train
    add train fct

---
+++ src/model/train.py
@@ -0,0 +1,53 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import numpy as np
+import cv2
+from glob import glob
+from sklearn.utils import shuffle
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, EarlyStopping, TensorBoard
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import Recall, Precision
+
+def shuffling(x, y):
+    x, y = shuffle(x, y, random_state=42)
+    return x, y
+
+def load_data(path):
+    x = sorted(glob(f"{path}/image/*.png"))
+    y = sorted(glob(f"{path}/mask/*.png"))
+    return x, y
+
+def read_image(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_COLOR)
+    x = x/255.0
+    x = x.astype(np.float32)
+    return x
+
+def read_mask(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    x = x/255.0
+    x = x > 0.5
+    x = x.astype(np.float32)
+    x = np.expand_dims(x, axis=-1)
+    return x
+
+def tf_parse(x, y):
+    def _parse(x, y):
+        x = read_image(x)
+        y = read_mask(y)
+        return x, y
+
+    x, y = tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])
+    x.set_shape([H, W, 3])
+    y.set_shape([H, W, 1])
+    return x, y
+
+def tf_dataset(x, y, batch=8):
+    dataset = tf.data.Dataset.from_tensor_slices((x, y))
+    dataset = dataset.map(tf_parse)
+    dataset = dataset.batch(batch)
+    dataset = dataset.prefetch(10)
+    return dataset
