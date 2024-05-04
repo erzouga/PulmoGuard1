097d6595f32ebfa76c46defd1d1dcb28440f3842
erzouga <erzouga10@gmail.com>
2024-05-04 08:26:49 +0100

    Creat eval.py
    image mask pred

---
+++ src/model/eval.py
@@ -0,0 +1,77 @@
+import os
+import numpy as np
+from tensorflow.keras.utils import CustomObjectScope
+import cv2
+
+from metrics import *
+from glob import glob
+
+from tqdm import tqdm
+
+def create_dir(path):
+    """ Create a directory. """
+    if not os.path.exists(path):
+        os.makedirs(path)
+H=512
+W=512
+def save_results(image, mask, y_pred, save_image_path):
+    ## i - m - y
+    line = np.ones((H, 10, 3)) * 128
+
+    """ Mask """
+    mask = np.expand_dims(mask, axis=-1)    ## (512, 512, 1)
+    mask = np.concatenate([mask, mask, mask], axis=-1)  ## (512, 512, 3)
+
+    """ Predicted Mask """
+    y_pred = np.expand_dims(y_pred, axis=-1)    ## (512, 512, 1)
+    y_pred = np.concatenate([y_pred, y_pred, y_pred], axis=-1)  ## (512, 512, 3)
+    y_pred = y_pred * 255
+
+    cat_images = np.concatenate([image, line, mask, line, y_pred], axis=1)
+    cv2.imwrite(save_image_path, cat_images)
+
+
+if __name__ == "__main__":
+    """ Seeding """
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+    """ Directory for storing files """
+    create_dir("results")
+
+    """ Loading model """
+    with CustomObjectScope({'iou': iou, 'dice_coef': dice_coef, 'dice_loss': dice_loss}):
+        model = tf.keras.models.load_model("files/model.h5")
+
+    """ Load the dataset """
+    test_x = sorted(glob(r"C:\Users\erzou\Desktop\lUNG_CANCER\eval_data\image1\*"))
+    test_y = sorted(glob(r"C:\Users\erzou\Desktop\lUNG_CANCER\eval_data\mask1\*"))
+    print(f"Test: {len(test_x)} - {len(test_y)}")
+
+    SCORE = []
+    for x, y in tqdm(zip(test_x, test_y), total=len(test_x)):
+        """ Extract the name """
+        name = x.split("\\")[-1].split(".")[0]
+        print(name)
+        """ Reading the image """
+        image = cv2.imread(x, cv2.IMREAD_COLOR)
+        x = image / 255.0
+        x = np.expand_dims(x, axis=0)
+
+        """ Reading the mask """
+        mask = cv2.imread(y, cv2.IMREAD_GRAYSCALE)
+        y = mask / 255.0
+        y = y > 0.5
+        y = y.astype(np.int32)
+
+        """ Prediction """
+        y_pred = model.predict(x)[0]
+        y_pred = np.squeeze(y_pred, axis=-1)
+        y_pred = y_pred > 0.5
+        y_pred = y_pred.astype(np.int32)
+
+        """ Saving the prediction """
+        save_image_path = f"results/{name}.png"
+        save_results(image, mask, y_pred, save_image_path)
+
+
